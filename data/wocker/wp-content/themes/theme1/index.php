e06df90b3945e1c29502e5b68db14642cb45d128
松永健太@SYS <matsunaga-kenta@dmm.com>
2015-09-14 18:39:27 +0900

    wordpress theme作成 temp

---
+++ data/wocker/wp-content/themes/theme1/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no">
+        <meta name="description" content="“tixeebox”（ティクシーボックス）は電子チケット発券のみに特化し、システム開発不要の主催者に導入しやすく、ユーザーにも利用しやすい電子チケットアプリサービスです。 ">
+        <link rel="canonical" href="https://tixeebox.tv/">
+        <link rel="shortcut icon" href="favicon.ico">
+        <link rel="icon" type="image/gif" href="animated_favicon1.gif">
+        <title>tixeebox（ティクシーボックス）| スマホチケット発券アプリ </title>
+        <link href="<?php bloginfo( 'template_directory' ); ?>/css/common.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+<body id="fn-hesiveTop">
+    
+
+    <div class="parallax-container">
+        <div class="parallax-logo hidden-xs">
+            <img src="<?php bloginfo( 'template_directory' ); ?>/img/logo2.png" alt="tixeeboxロゴ">
+        </div>
+        <div class="parallax">
+            <img src="<?php bloginfo( 'template_directory' ); ?>/img/key_visual1.png" alt="tixeeboxイメージ">
+        </div>
+    </div>
+
+    <div class="header">
+    <nav class="hidden-xs">
+        <ul class="left hide-on-med-and-down">
+            <li><a target="_blank" href="http://livestyles.tv/news">NEWS</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/aboutus">ABOUT US</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/services">SERVICES</a></li>
+            <li><a href="/lp/how-to/">HOW TO</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/patent">PATENT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/recruit">RECRUIT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/contact">CONTACT</a></li>
+        </ul>
+        <ul class="social-icons">
+            <li>
+                <a href="https://twitter.com/tixeebox1" target="_blank">
+                    <i class="icon-twitter"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <nav class="visible-xs navbar-fixed-top">
+        <ul class="right hide-on-med-and-down">
+            <li><a target="_blank" href="http://livestyles.tv/news">NEWS</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/aboutus">ABOUT US</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/services">SERVICES</a></li>
+            <li><a href="/lp/how-to/">HOW TO</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/patent">PATENT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/recruit">RECRUIT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/contact">CONTACT</a></li>
+        </ul>
+        <ul id="slide-out" class="side-nav">
+            <li><a target="_blank" href="http://livestyles.tv/news">NEWS</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/aboutus">ABOUT US</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/services">SERVICES</a></li>
+            <li><a href="/lp/how-to/">HOW TO</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/patent">PATENT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/recruit">RECRUIT</a></li>
+            <li><a target="_blank" href="http://livestyles.tv/contact">CONTACT</a></li>
+        </ul>
+        <a data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+        <a href="#top" class="brand-logo center"><img src="img/logo-white.png" alt="tixeeboxロゴ"></a>
+        <ul class="social-icons">
+            <li>
+                <a href="https://twitter.com/tixeebox1" target="_blank">
+                    <i class="icon-twitter"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+    <div class="wrapper">
+        <section class="row container">
+            <a id="fn-showHere"></a>
+            <div class="header u-text-center u-pt30">
+                <img src="/<?php bloginfo( 'template_directory' ); ?>/img/logo.png" alt="tixeeboxロゴ">
+            </div>
+            <h2 class="heading-a u-text-center">
+                電子チケットの発券、及び入場管理！<br>
+                スマホユーザーに新たな電子チケット入場体験を！
+            </h2>
+            <p class="grey-text text-darken-3 lighten-3">
+                “tixeebox”（ティクシーボックス）は電子チケットの発券に特化し、システム開発不要の主催者に導入しやすく、ユーザーにも利用しやすい電子チケットアプリサービスです。tixeeboxでは転売の抑制としてSMSでのユーザー認証、チケット譲渡の制限機能を備えている他、電子チケットならではのチケットデザイン、動画などのコンテンツの付与にも対応可能です！
+            </p>
+            <p class="grey-text text-darken-3 lighten-3">
+                従来は紙で受取っていたチケットを、“tixeebox”アプリなら簡単に電子化＆スマホにダウンロード！コンビニや店頭に、チケットを受取に行く必要はありません。 イベント当日は、スマートフォンを見せてチケットを「もぎって」もらうだけ！紙チケットと同じ方法で入場できます！
+            </p>
+        </section>
+        <section class="row container u-pt60">
+            <div class="col s6">
+                <section class="card">
+                    <h1 class="heading-b">チケットを簡単に<br>スマホで受け取り</h1>
+                    <div class="card-image">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/image1.png">
+                    </div>
+                </section>
+            </div>
+            <div class="col s6">
+                <section class="card">
+                    <h1 class="heading-b">入場方法は<br>チケット画面を"もぎる"だけ！</h1>
+                    <div class="card-image">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/image2.png">
+                    </div>
+                </section>
+            </div>
+        </section>
+        <section class="row container">
+            <div class="col s6">
+                <section class="card">
+                    <h1 class="heading-b">友達にLINEやメールで<br>チケットを送付可能</h1>
+                    <div class="card-image">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/image3.png">
+                    </div>
+                </section>
+            </div>
+            <div class="col s6">
+                <section class="card">
+                    <h1 class="heading-b">発券手数料は0円<br>アカウント登録も無料！</h1>
+                    <div class="card-image">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/image4.png">
+                    </div>
+                </section>
+            </div>
+        </section>
+        <section class="row container u-pt60">
+            <div class="col s12">
+                <div class="thumbnail u-text-center u-mb30">
+                    <img src="<?php bloginfo( 'template_directory' ); ?>/img/ticket_flow.png" alt="チケットの流れ">
+                </div>
+
+                <p class="grey-text text-darken-3 lighten-3">
+                    tixeeboxはスマートフォン向けの電子チケット発券に特化したツールで、主催者、プレイガイドが販売したチケットの電子化が行えます。これまでの電子チケットとは異なり、販売(決済)は自社、発券はtixeeboxで行えるため既存のシステムに容易に電子チケット発券を組み込むことが出来ます。CSVでの連携の他、APIでの連携にも対応しているので、システムを繋ぎ込めば継続的かつ、リアルタイムに電子チケットの発券をすることが可能になります。
+                </p>
+            </div>
+        </section>
+        <section class="row container u-pt60 u-pb60">
+            <div class="col s12">
+                <div class="thumbnail u-text-center">
+                    <div class="visible-xs">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/contact-sp.png" alt="お問い合わせ">
+                    </div>
+                    <div class="hidden-xs">
+                        <img src="<?php bloginfo( 'template_directory' ); ?>/img/contact.png" alt="お問い合わせ">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+                    <!--フッター-->
+                <footer class="footer">
+                    <div class="container">
+                        ©2015 LiveStyles, Inc. All Rights Reserved.
+                    </div>
+                </footer>
+            <script src="<?php bloginfo( 'template_directory' ); ?>/js/lib.min.js"></script>
+        <script src="<?php bloginfo( 'template_directory' ); ?>/js/all.min.js"></script>
+        <script src="<?php bloginfo( 'template_directory' ); ?>/js/common.min.js"></script>
+    </body>
+</html>
