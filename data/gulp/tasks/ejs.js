cbd0313fe713717ba607e7beb9f43fc91488030b
松永 健太@SYS <matsunaga-kenta@dmm.com>
2015-11-11 18:33:29 +0900

    gulp分割完了

--- data/gulp/tasks/ejs.js
+++ data/gulp/tasks/ejs.js
@@ -6,12 +6,16 @@ var reload = browserSync.reload;
 var ejs = require("gulp-ejs");
 var plumber = require("gulp-plumber");
 var rename = require('gulp-rename');
+var fs = require('fs');
 
 
 //ejs
 gulp.task("ejs", function () {
+            // 最新のJSONファイルを同期読み込みしてオブジェクトを生成
+            var json = JSON.parse(fs.readFileSync("src/data/pages.json"));
+
             gulp.src([config.path.ejs.src.on, '!'+ config.path.ejs.src.off])
-                .pipe(ejs())
+                .pipe(ejs(json))
                 .pipe(plumber())
                 .pipe(rename({extname: '.php'}))
                 .pipe(gulp.dest(config.path.all.dest))
