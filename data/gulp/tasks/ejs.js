d80ee5396455030a8a27b7da9134e80aa0826b60
松永 健太@SYS <matsunaga-kenta@dmm.com>
2015-11-11 17:58:05 +0900

    commit

--- data/gulp/tasks/ejs.js
+++ data/gulp/tasks/ejs.js
@@ -6,14 +6,13 @@ var reload = browserSync.reload;
 var ejs = require("gulp-ejs");
 var plumber = require("gulp-plumber");
 var rename = require('gulp-rename');
-var fs = require('fs');//ファイルの読み込み
 
 //ejs
 gulp.task("ejs", function () {
+
             gulp.src([config.path.ejs.src.on, '!'+ config.path.ejs.src.off])
                 .pipe(plumber())
-                // オブジェクトを渡してデータの当て込み
-                .pipe(ejs(json))
+                .pipe(ejs())
                 .pipe(rename({extname: '.php'}))
                 .pipe(gulp.dest(config.path.all.dest))
                 .pipe(reload({stream: true}));
